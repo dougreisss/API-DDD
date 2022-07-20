2cd85291f4c7f78fba4f340da293989ecb58dbe2
Douglas Reis <douglas_rrs@hotmail.com>
2022-07-19 21:27:16 -0300

    CORS

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -39,6 +39,7 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddDbContext<Contexto>(options =>
              options.UseSqlServer(
                  Configuration.GetConnectionString("DefaultConnection")));
@@ -99,6 +100,10 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var urlCliente1 = "";
+
+            app.UseCors(b => b.WithOrigins(urlCliente1));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
